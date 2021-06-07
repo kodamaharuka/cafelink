9c8ebbaee1f0ee38c3f1d48082f67cf6f2a7493c
kodamaharuka <mkhrauau@gmail.com>
2021-06-07 08:22:49 +0000

    0607

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,7 +3,6 @@ class Public::CommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    #comment = Comment.new(comment_paramas)
     comment = current_user.comments.new(comment_params)
     comment.user_id = current_user.id
     comment.post_id = post.id
